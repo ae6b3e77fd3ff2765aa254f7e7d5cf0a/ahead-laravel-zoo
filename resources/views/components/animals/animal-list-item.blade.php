83c2b3bb7adc559646495b375b37c8c56ffab78d
codeine <u@s.d>
2024-10-30 07:14:44 +0000

    fix other bugs

--- resources/views/components/animals/animal-list-item.blade.php
+++ resources/views/components/animals/animal-list-item.blade.php
@@ -3,9 +3,6 @@
     <td><a class="link-info"
            href="{{ route('animals.show', $id) }}">{{ $name }}</a>
     </td>
-    <td>
-        {{ $hasCage ? 'Да' : 'Нет' }}
-    </td>
     @auth
         <td>
             <x-forms.edit-form-inline :id="$id" base="animals"></x-forms.edit-form-inline>
